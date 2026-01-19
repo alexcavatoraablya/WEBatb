07714c7231442f8c384e05866e05d258fb1afecc
CODER <CODER@rivne.itstep.org>
2026-01-19 13:09:56 +0200

    zrobutu dz

---
+++ WebATB/WebATB/Controllers/MainController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebATB.Models.Users;
+
+namespace WebATB.Controllers;
+
+public class MainController : Controller
+{
+    public IActionResult Index()
+    {
+        List<UserItemModel> model = new List<UserItemModel>();
+        model.Add(new UserItemModel
+        {
+            Id = 1,
+            Name = "Вікторович Віктор Йосипович",
+            Phone = "098221546781"
+        });
+
+        model.Add(new UserItemModel
+        {
+            Id = 1,
+            Name = "Рижий Марко Йосипович",
+            Phone = "098564783209"
+        });
+
+        model.Add(new UserItemModel
+        {
+            Id = 1,
+            Name = "Галоша Іванна Ігорівна",
+            Phone = "098264284327"
+        });
+
+        return View(model);
+    }
+}
